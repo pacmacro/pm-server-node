50eff6fd1baa29ed861209abd670ff0cb175c897
Jeffrey Leung <jleung513@gmail.com>
2019-02-22 13:57:36 -0800

    Add logging, Access headers, 500 failures

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ const serverOptions = {
 function setMappings(app) {
   app.use(bodyParser.json());
   app.use(express.static("public"));
+
+  app.all("*", function(req, res, next) {
+    console.log(`[INFO] Mapped ${req.method} ${req.originalUrl}`);
+
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+
+    res.type("json");
+
+    try {
+      next();
+    } catch (except) {
+      console.log(`[ERROR] ${except}`);
+      res.status(500);
+    }
+
+    console.log(`[INFO] Replying to request with HTTP ${res.statusCode}`);
+    res.end();
+});
+
+
   app.get("/", function (req, res) {
     res.status(200).send("Hello World!");
   });
