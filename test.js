6b081c1dbea9a68a3b471856b6c229d6c372abf5
Caleb Whitehead <whiitehead@gmail.com>
2019-11-20 21:40:46 -0800

    removes old files

--- test.js
+++
@@ -1,5 +0,0 @@
-const obj = { greet: "hi", your: "mom" }
-const obj2 = { greet: "hi", your: "dad" }
-const contents = { greet: "hi", your: "mo" }
-// prettier-ignore
-console.log(Object.keys(contents).reduce((previous, current) => previous && obj[current] === contents[current], true))
